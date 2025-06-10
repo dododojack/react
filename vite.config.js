e7e14616bfcfbe3ec149a692169941c643aed3c6
dododojack <109461651+dododojack@users.noreply.github.com>
2025-06-11 01:50:23 +0800

    更新個人資料

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/react/', // ← 這個很關鍵！
+  base: '/react/',  // ← 根據你的 GitHub repo 名稱設定
 })
